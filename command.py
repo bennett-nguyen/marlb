aaf71506f965b6b204bf71bcac6f3eddb559345c
bennett-nguyen <83117848+bennett-nguyen@users.noreply.github.com>
2021-12-16 14:01:43 +0700

    Update command.py

--- command.py
+++ command.py
@@ -1,50 +1,61 @@
 from time import sleep
 
 # !
-def stop_exec(*args):
+def stop_exec(*args, **kwargs):
+    if kwargs["mode"] == "debug":
+        print("sleeped for 0.5 seconds")
+    
     sleep(0.5)
 
 
 # -
-def decrease_pitch(info):
+def decrease_pitch(info, mode):
     if not info["pitch_level"] - 1:
         print(f"Lowest level of pitch reached: {info['pitch_level']}")
         return
 
     info["pitch_level"] = info["pitch_level"] - 1
-
-    print(f"Decreased pitch to level: {info['pitch_level']}")
+    
+    if mode == "debug":
+        print(f"Decreased pitch to level: {info['pitch_level']}")
 
 # + 
-def increase_pitch(info):
-    if info["pitch_level"] + 1 == 17:
+def increase_pitch(info, mode):
+    if info["pitch_level"] + 1 == 17 and mode == "debug":
         print(f"Reached highest pitch possible: {info['pitch_level']}")
         return
 
     info["pitch_level"] = info["pitch_level"] + 1
 
-    print(f"Increased pitch to level: {info['pitch_level']}")
+    if mode == "debug":
+        print(f"Increased pitch to level: {info['pitch_level']}")
 
 # <
-def decrease_duration(info):
-    if info["duration"] - 100 < 0:
+def decrease_duration(info, mode):
+    if info["duration"] - 100 < 0 and mode == "debug":
         print(f"Reached lowest duration possible: {info['duration']}ms")
         return
 
     info["duration"] = info["duration"] - 100
-
-    print(f"Decreased duration to: {info['duration']}ms")
+    
+    if mode == "debug":
+        print(f"Decreased duration to: {info['duration']}ms")
 
 # >
-def increase_duration(info):
-    if info["duration"] + 100 > 15000:
+def increase_duration(info, mode):
+    if info["duration"] + 100 > 15000 and mode == "debug":
         print(f"Reached highest duration possible: {info['duration']}ms")
         return
 
     info["duration"] = info["duration"] + 100
-    print(f"Increased duration to: {info['duration']}ms")
+    
+    if mode == "debug":
+        print(f"Increased duration to: {info['duration']}ms")
 
-def reset(info):
+# :
+def reset(info, mode):
     info["duration"] = 200
     info["pitch_level"] = 2
-    print("Set duration and pitch level to default")
+    
+    if mode == "debug":
+        print("Set duration and pitch level to default")
